ba5ac2e1b8f77fe6625b3910f5a5f8750c84d2b6
EGI-dev <egi.startup@gmail.com>
2024-08-09 15:26:27 +0200

    expandable projects

--- scripts.js
+++ scripts.js
@@ -8,10 +8,17 @@ function scrollToSection(sectionId) {
     }
 }
 
+// Function to return to the home page at a desired section
+function goToMainPage(sectionId) {
+    window.location.href = '../index.html#'+ sectionId;
+}
+
 // Event listener for carousel images
 document.querySelectorAll('.carousel-images img').forEach(image => {
     image.addEventListener('click', () => {
         const text = image.getAttribute('data-text');
-        document.getElementById('carousel-text').textContent = text;
+        const url = image.getAttribute('data-url');
+        const buttonHtml = '<br><br><button class="outline contrast" onclick="window.location.href=\'' + url + '\'">Learn More</button>';
+        document.getElementById('carousel-text').innerHTML = text + buttonHtml;
     });
 });
